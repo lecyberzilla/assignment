13325b359e902f64a1fc31e97a0e720f955100ba
Ansari <63479371+lecyberzilla@users.noreply.github.com>
2021-06-03 22:02:46 +0500

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+int fd1;
+char * fifo_one = "/tmp/fifo_one";
+mkfifo(fifo_one, 0666);
+char buffer[80];
+
+fd1 = open(fifo_one, O_WRONLY);
+write(fd1, buffer, sizeof(buffer)); 
+printf("Saqlain Nasir mc200204513\n");
+closed(fd1);
+
+}
