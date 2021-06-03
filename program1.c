331e6769bf3add3fcd5d9ac4ba8889d8ae89b0d3
Ansari <63479371+lecyberzilla@users.noreply.github.com>
2021-06-03 22:19:35 +0500

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+ 
+
+int main()
+{
+int fd; 
+char * fifo_one = "/tmp/fifo_one";
+mkfifo(fifo_one, 0666);
+char buffer[80];
+ 
+fd = open(fifo_one, O_RDONLY);
+read(fd, buffer, sizeof(buffer));
+
+closed(fd); 
+
+}
